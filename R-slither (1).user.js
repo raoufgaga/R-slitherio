8be1918803f28acea8f6469a00d74df29d3318a7
raoufgaga <raoufgame2@gmail.com>
2016-06-21 20:55:24 +0200

    Add files via upload

---
+++ R-slither (1).user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         R/slither
+// @namespace    www.manific.tk
+// @version      3
+// @description  www.manific.tk
+// @author       www.manific.tk
+// @require      http://code.jquery.com/jquery-latest.js
+// @match        http://slither.io/
+// @run-at document-start
+// @grant        GM_xmlhttpRequest
+// ==/UserScript==
+
+var s = document.createElement('script');
+s.src = 'https://code.jquery.com/jquery-1.12.3.min.js';
+s.onload = function() {
+	document.head.innerHTML += '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">';
+	var s = document.createElement('script');
+	s.src = 'http://yourjavascript.com/6236151221/slitherplus.js';
+	s.onload = function() {
+		this.parentNode.removeChild(this);
+	};
+	(document.head || document.documentElement).appendChild(s);
+};
+(document.head || document.documentElement).appendChild(s);
